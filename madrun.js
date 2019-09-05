647124fc6e33bbefdf21f8db36497252b57ae28c
coderaiser <coderaiser@cloudcmd.io>
2019-09-05 16:35:57 +0300

    test(echo-at-time) add

--- madrun.js
+++ madrun.js
@@ -4,7 +4,8 @@ const {run} = require('madrun');
 
 module.exports = {
     'start': () => `node bin/echo-at-time.js`,
-    'test': () => `tape 'test/*.js'`,
+    'test': () => `tape 'lib/**/*.spec.js'`,
+    'watch:test': () => `nodemon -w lib -x ${run('test')}`,
     'lint': () => `putout bin lib madrun.js`,
     'fix:lint': () => run('lint', '--fix'),
     'coverage': () => `nyc ${run('test')}`,
